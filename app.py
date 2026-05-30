b04d0b7bbd8a0d611151e0327cefcd151b36cbe1
Sasha-Tameka <sashh92@hotmail.com>
2026-05-30 16:12:59 -0400

    app.py created

---
+++ app.py
@@ -0,0 +1,16 @@
+#create app
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.routr("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
