78847835f0fa2e4b7da9a693cd4f4cd2ac82e999
Sasha-Tameka <sashh92@hotmail.com>
2026-06-15 16:51:30 -0400

    added emailjs

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-@import url('')
+/*EmailJS */
+emailjs.init("UYRy36jT937LKCpWn");
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const status = document.getElementById("form-status");
+    status.textContent = "Sending...";
+
+    emailjs
+      .sendForm("service_8zkrcgo", "template_jqxqc9c", this)
+      .then(() => {
+        status.textContent = "Message sent successfully!";
+        status.style.color = "4bb7744";
+        this.resizeTo();
+      })
+
+      .catch(() => {
+        status.textContent = "Something went wrong. Please try again.";
+        status.style.color = "b74b4b";
+      });
+  });
